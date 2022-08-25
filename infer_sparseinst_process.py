a721df3100464583adf3532a411ce7900ec7bff6
ambroise <ambroise.berthe@ikomia.com>
2022-08-25 12:21:44 +0200

    Fix class names when not in cfg

--- infer_sparseinst_process.py
+++ infer_sparseinst_process.py
@@ -149,7 +149,21 @@ class InferSparseinst(dataprocess.C2dImageTask):
                 self.args.input = ""
                 self.args.config_file = os.path.join(plugin_folder, "configs", "sparse_inst_r50_giam_aug.yaml")
                 cfg = self.setup_cfg(self.args)
-                self.class_names = cfg.CLASS_NAMES
+                try:
+                    self.class_names = cfg.CLASS_NAMES
+                except AttributeError:
+                    self.class_names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+                                        'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
+                                        'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+                                        'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag',
+                                        'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite',
+                                        'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+                                        'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana',
+                                        'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+                                        'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table',
+                                        'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+                                        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+                                        'scissors', 'teddy bear', 'hair drier', 'toothbrush']
                 param.update = False
                 self.model = VisualizationDemo(cfg).predictor
 
