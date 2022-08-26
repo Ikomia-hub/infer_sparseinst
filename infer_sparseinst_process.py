2e9657d215fe1aae4e297b49cfc25fcf2a3bdd0d
ambroise <ambroise.berthe@ikomia.com>
2022-08-26 10:32:19 +0200

    Fix no module named pywin32

--- infer_sparseinst_process.py
+++ infer_sparseinst_process.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from infer_sparseinst import update_path
 from ikomia import core, dataprocess
 import copy
 # Your imports below
